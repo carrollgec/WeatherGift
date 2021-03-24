d31a2cb42e65c86d91314c0df9fa21a5be4def2b
Grace <carrolhn@bc.edu>
2021-03-24 14:02:20 -0400

    Completed 6.11

--- WeatherGift/WeatherLocation.swift
+++ WeatherGift/WeatherLocation.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct WeatherLocation: Codable {
+class WeatherLocation: Codable {
     var name: String
     var latitude: Double
     var longitude: Double
@@ -17,34 +17,6 @@ struct WeatherLocation: Codable {
         self.latitude = latitude
         self.longitude = longitude
         
-    }
-    
-    func getData() {
-        let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
-        
-        print("Accessing the url \(urlString)")
-        
-        // create a url
-        guard let url = URL(string: urlString) else {
-            print("ERROR: Could not create a url from \(urlString)")
-            return
-        }
-        // create session
-        let session = URLSession.shared
-        
-        // get data with .datatask
-        let task = session.dataTask(with: url) { (data, response, error) in
-            if let error = error {
-                print("ERROR \(error.localizedDescription)")
-            }
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                print("\(json)")
-            } catch {
-                print("ERROR JSON")
-            }
-        }
-        task.resume()
     }
     
 }
